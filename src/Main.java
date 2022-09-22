e5dcb3eb7de3fe7d486837bdbf8d4204285edf72
Igoryok8 <energymag@mail.ru>
2022-09-22 23:10:55 +0300

    Задание 6 выполнено

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var weightBoxerOne = 78.2;
+
+        System.out.println("Вес одного боксёра " + weightBoxerOne + " кг.");
+        var weightBoxerTwo = 82.7;
+        System.out.println("Вес второго боксёра " + weightBoxerTwo + " кг.");
+        var totalWeight = weightBoxerOne + weightBoxerTwo;
+        System.out.println("Общий вес боксёров " + totalWeight + " кг.");
+        var differenceWeight = weightBoxerTwo - weightBoxerOne;
+        System.out.println("Разница в весе боксёров " + differenceWeight + " кг.");
+    }
+}
